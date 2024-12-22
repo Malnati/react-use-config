c8e5516a0fc9e78edd611a00cce3048597999d60
Ricardo Malnati <35036961+Malnati@users.noreply.github.com>
2024-12-22 10:02:15 -0300

    Main (#17)

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
+// jest.config.js
+
+const { defaults } = require('ts-jest/presets');
+
 module.exports = {
-	testEnvironment: 'jest-environment-jsdom',
-	transform: {
-	  '^.+\\.tsx?$': 'ts-jest'
-	},
-	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
-  };
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    ...defaults.transform,
+  },
+  transformIgnorePatterns: [
+    '/node_modules/', // Ignorar transformações em node_modules
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFiles: ['./jest.env.js'], // Configuração para carregar variáveis de ambiente
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
